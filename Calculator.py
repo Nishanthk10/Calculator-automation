a3a3e179445acbef0aae93ac62a2884ccfe29e90
nishanth <nishanthsentyar@gmail.com>
2023-02-26 17:48:40 +0530

    added test_cases.py

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,13 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+
 while True:
     print("1.Addition 2.Substraction 3.Multiplication")
     choice = int(input("Enter your choice: "))
@@ -5,11 +15,11 @@ while True:
     b = int(input("Enter second number: "))
 
     if choice == 1:
-        print("The result is ",a+b)
+        print("The result is ",add(a,b))
     elif choice ==2:
-        print("The result is ",a-b)
+        print("The result is ",sub(a,b))
     elif choice ==3:
-        print("The result is ",a*b)
+        print("The result is ",mult(a,b))
     else:
         print("Invalid input")
 
