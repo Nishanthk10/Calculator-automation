c74a9b289b9e0ffa6cb9ff001c15144d60707a60
nishanth <nishanthsentyar@gmail.com>
2023-02-26 23:40:04 +0530

    Commented out few things

--- Calculator.py
+++ Calculator.py
@@ -8,18 +8,18 @@ def mult(a,b):
     return a*b
 
 
-while True:
-    print("1.Addition 2.Substraction 3.Multiplication")
-    choice = int(input("Enter your choice: "))
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
+# while True:
+#     print("1.Addition 2.Substraction 3.Multiplication")
+#     choice = int(input("Enter your choice: "))
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
 
-    if choice == 1:
-        print("The result is ",add(a,b))
-    elif choice ==2:
-        print("The result is ",sub(a,b))
-    elif choice ==3:
-        print("The result is ",mult(a,b))
-    else:
-        print("Invalid input")
+#     if choice == 1:
+#         print("The result is ",add(a,b))
+#     elif choice ==2:
+#         print("The result is ",sub(a,b))
+#     elif choice ==3:
+#         print("The result is ",mult(a,b))
+#     else:
+#         print("Invalid input")
 
